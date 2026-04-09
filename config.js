6c17f838194ee19f51f5488bcf234cdaf92ecee1
PadhleYrr <moliage490@gmail.com>
2026-04-09 15:16:11 +0530

    unzipgit

---
+++ config.js
@@ -0,0 +1,5 @@
+const UNREAL_CONFIG = {
+  GOOGLE_API_KEY:  "AIzaSyBwz1CUfFgjKmBbnqMnXZdiRYOf9lEHh_o",
+  MONGODB_URI:     "mongodb+srv://YOUR_USER:YOUR_NEW_PASSWORD@cluster0.mojpz.mongodb.net/visionai?retryWrites=true&w=majority",
+  RENDER_NODE_URL: "",
+};
