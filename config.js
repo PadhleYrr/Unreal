a525582c2ccf1169ec191debc382c8f61a0c773d
PadhleYrr <moliage490@gmail.com>
2026-04-09 15:21:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 const UNREAL_CONFIG = {
   GOOGLE_API_KEY:  "AIzaSyBwz1CUfFgjKmBbnqMnXZdiRYOf9lEHh_o",
-  MONGODB_URI:     "mongodb+srv://YOUR_USER:YOUR_NEW_PASSWORD@cluster0.mojpz.mongodb.net/visionai?retryWrites=true&w=majority",
+  MONGODB_URI:     "mongodb+srv://xaayux:909090@cluster0.mojpz.mongodb.net/visionai?retryWrites=true&w=majority",
   RENDER_NODE_URL: "",
 };
+
